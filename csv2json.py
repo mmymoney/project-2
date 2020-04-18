be6f8d883fee5493e533d58a776b99f982d2ebbd
Mike Mymoney <57593577+mmymoney@users.noreply.github.com>
2020-04-17 19:14:58 -0700

    updates

---
+++ csv2json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import csv
+import json
+import sys
+
+tree = {}
+
+reader = csv.reader(open(sys.argv[1], 'rb'))
+reader.next() 
+for row in reader:
+    subtree = tree
+    for i, cell in enumerate(row):
+        if cell:
+            if cell not in subtree:
+                subtree[cell] = {} if i<len(row)-1 else 1
+            subtree = subtree[cell]
+
+print json.dumps(tree, indent=2)
+
